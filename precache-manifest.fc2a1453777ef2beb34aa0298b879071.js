2a85123356ad6eea1afd2a1b0605a43c53a06ae2
Punpikorn <sadanthem@gmail.com>
2018-11-23 00:12:47 +0000

    Updates

---
+++ precache-manifest.fc2a1453777ef2beb34aa0298b879071.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3eec4bc9dd04a1db8669",
+    "url": "/projectxi-preview/static/js/runtime~main.3eec4bc9.js"
+  },
+  {
+    "revision": "ca82b9ba4f414fb3ba34",
+    "url": "/projectxi-preview/static/js/main.ca82b9ba.chunk.js"
+  },
+  {
+    "revision": "b3714e2fab22cebdccc6",
+    "url": "/projectxi-preview/static/js/1.b3714e2f.chunk.js"
+  },
+  {
+    "revision": "ca82b9ba4f414fb3ba34",
+    "url": "/projectxi-preview/static/css/main.6f8657ab.chunk.css"
+  },
+  {
+    "revision": "b3714e2fab22cebdccc6",
+    "url": "/projectxi-preview/static/css/1.19585c2c.chunk.css"
+  },
+  {
+    "revision": "a51a63cbb7e7c69b7fab445b7d7a6900",
+    "url": "/projectxi-preview/index.html"
+  }
+];
